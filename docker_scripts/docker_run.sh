fa05fb96e7eb4c48caf28c301b14562d3b529f4a
MINGUKKANG <first287@naver.com>
2021-02-02 03:08:26 +0000

    backup

--- docker_scripts/docker_run.sh
+++ docker_scripts/docker_run.sh
@@ -1,15 +1,11 @@
 #docker build -t research_img --build-arg local_xauth_="$localxauth_var" --build-arg local_display="$displayvar" .
 
-docker run -it --runtime=nvidia --ipc=host --net=host \
-       --privileged \
-       --env DISPLAY \
-       --volume /playpen/john/docker_scripts/.vimrc:/root/.vimrc \
-       --volume /tmp/.X11-unix:/tmp/.X11-unix \
-       --volume /var/run/docker.sock:/var/run/docker.sock \
-       --env XAUTHORITY=/root/.Xauthority \
-       -v /playpen/john/:/root/code \
-       -v /data/:/root/data \
-       --env NVIDIA_DRIVER_CAPABILITIES=compute,utility \
-       mgkang/studio_gan:latest bash
 
-#
+docker run -it --gpus all \
+        --shm-size 256g -p 6006:6006 \
+        -v /playpen/john/:/root/code \
+        -v /data/:/root/data \
+        --volume /playpen/john/docker_scripts/.vimrc:/root/.vimrc \
+        --workdir /root/code \
+        mgkang/studio_gan:latest /bin/zsh
+
